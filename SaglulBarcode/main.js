dc76b12ffd8fc7d9d45223de2619541f846032fa
Dana Iranpanah <55066922+iran0017@users.noreply.github.com>
2021-09-27 21:55:07 -0400

    Add files via upload

---
+++ SaglulBarcode/main.js
@@ -0,0 +1,29 @@
+
+
+Quagga.init({
+  inputStream: {
+    name: "Live",
+    type: "LiveStream",
+    target: document.getElementById('video-wrapper')
+  },
+  decoder: {
+    readers: ["code_128_reader"]
+  }
+}, function (err) {
+  if (err) {
+    console.log(err);
+    return
+  }
+  console.log("Initialization finished. Ready to start");
+  Quagga.start();
+});
+
+
+Quagga.onDetected(barcodeDetected)
+function barcodeDetected(data) {
+  document.getElementById('quaggaVideo').pause()
+  console.log('detectedObject', data);
+  alert(data.codeResult.code)
+  Quagga.stop()
+}
+
