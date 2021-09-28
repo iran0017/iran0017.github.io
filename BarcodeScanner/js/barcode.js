f0f3f981d7234b2583f0d58457304ef2f4a53aab
Dana Iranpanah <55066922+iran0017@users.noreply.github.com>
2021-09-27 22:52:25 -0400

    Add files via upload

--- BarcodeScanner/js/barcode.js
+++ BarcodeScanner/js/barcode.js
@@ -1,153 +1,32 @@
-if (!('BarcodeDetector' in window)) { alert('Barcode detector is not supported in this OS!') }
-var streamObj;
-var streamID;
-// Get video element 
-const video = document.getElementById('video');
-let codes = [];
-let seen = new Set();
-var audio = new Audio('/BarcodeScanner/sounds/beep.wav');
-let formats;
-// Save all formats to formats var 
-BarcodeDetector.getSupportedFormats().then(item => formats = item);
-// Create new barcode detector
-const barcodeDetector = new BarcodeDetector({ formats: formats });
 
-// Define custom element
-customElements.define('scaned-item', class extends HTMLElement {
-    constructor() {
-        super();
-        const template = document.querySelector('#scaned-item').content;
-        const shadowRoot = this.attachShadow({ mode: 'open' }).appendChild(template.cloneNode(true));
-    };
-});
-
-// Codes proxy/state
-const codesProxy = new Proxy(codes, {
-    set(target, prop, value, receiver) {
-        // Throw err if value is a number
-        // Stops from saving undefined codes
-        if (typeof value === 'number') throw value;
-
-        target.push(value);
-
-        // Check if code has already been scanned
-        target = target.filter((c) => {
-            if (c.rawValue !== window.barcodeVal) return c;
-            const d = seen.has(c.rawValue);
-            seen.add(c.rawValue);
-            return !d;
-        })
-        printBarcodeOutput(value.rawValue)
-        return true;
-    }
-});
-const printBarcodeOutput = (value) => {
-    console.info('Barcode result is: ', value);
-    audio.play();
-    alert(value)
-}
-function stopBothVideoAndAudio(stream) {
-    stream.getTracks().forEach(function (track) {
-        if (track.readyState == 'live') {
-            track.stop();
+var audio = new Audio('/dist/sounds/beep.wav');
+function initBarcodeScanning(videoContainer) {
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.getElementById(videoContainer)
+        },
+        decoder: {
+            readers: ["code_128_reader"]
         }
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
     });
-}
-// Draw outline to canvas 
-const drawCodePath = ({ cornerPoints }) => {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    const strokeGradient = ctx.createLinearGradient(0, 0, canvas.scrollWidth, canvas.scrollHeight);
-    console.log('cornerPoints:', cornerPoints.entries(), canvas.scrollWidth, canvas.scrollHeight)
-    // Exit function and clear canvas if no corner points
-    //if (!cornerPoints) return ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Clear canvas for new redraw
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Create new gradient
-    strokeGradient.addColorStop('0', '#c471ed');
-    strokeGradient.addColorStop('1', '#f7797d');
-
-    // Define stoke styles
-    ctx.strokeStyle = strokeGradient;
-    ctx.lineWidth = 4;
-
-    // Begin draw
-    ctx.beginPath();
-    const xOffset = canvas.width / 4;
-    const yOffset = canvas.height / 4;
-    // Draw code outline path
-    for (const [i, { x, y }] of cornerPoints.entries()) {
-        console.log(i, x, y)
-        if (i === 0) {
-            // Move x half of the stroke width back 
-            // makes the start and end corner line up
-            ctx.moveTo(x + xOffset, y + yOffset);
-            continue;
-        }
-        // Draw line from current pos to x, y
-        ctx.lineTo(x + xOffset, y + yOffset);
-        // Complete square draw to starting position
-        if (i === cornerPoints.length - 1) ctx.lineTo(cornerPoints[0].x + xOffset, cornerPoints[0].y + yOffset);
+    Quagga.onDetected(barcodeDetected)
+    function barcodeDetected(data) {
+        document.getElementById('quaggaVideo').pause()
+        console.log('detectedObject', data);
+        audio.play();
+        alert(data.codeResult.code)
+        Quagga.stop()
     }
-
-    // Make path to stroke
-    ctx.stroke();
 }
-var isDetected = false;
-// Detect code function 
-const detectCode = () => {
-    barcodeDetector.detect(video).then(codes => {
-        // If no codes exit function and clear canvas
-        //if (codes.length === 0) return drawCodePath({});
 
-        for (const barcode of codes) {
-            console.log(barcode)
-            // Draw outline
-            video.pause();
-            isDetected = true;
-            //if (isDetected)
-            //drawCodePath(barcode);
-            // Code in seen set then exit loop 
-            if (seen.has(barcode.rawValue)) return;
-
-            // Save barcode to window to use later on
-            // then push to the codes proxy
-            window.barcodeVal = barcode.rawValue;
-            codesProxy.push(barcode);
-        }
-    }).catch(err => {
-        console.log(err);
-    })
-}
-function initScanBarcode(streamid) {
-    streamID = streamid;
-    // Check for a camera
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        const constraints = {
-            video: { facingMode: "environment" },
-            audio: false
-        };
-        // Start video stream
-        navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-            video.srcObject = stream
-            streamObj = stream;
-        });
-    }
-    if (!streamObj)
-        document.getElementById(streamID).addEventListener("hidden.bs.modal", function () {
-            isDetected = false;
-            seen = new Set();
-            if (streamObj)
-                stopBothVideoAndAudio(streamObj);
-        });
-    // Run detect code function every 100 milliseconds
-    let detectInterval = setInterval((interval) => {
-        detectCode();
-        if (isDetected) {
-            clearInterval(detectInterval);
-            stopBothVideoAndAudio(streamObj);
-        }
-    }, 100);
-}
